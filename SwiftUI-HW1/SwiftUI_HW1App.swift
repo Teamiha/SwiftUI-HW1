36e8227d5f43f3cbb377aee280cc8907cb12be37
Mikhail <jegnum@gmail.com>
2022-05-11 23:57:45 +0300

    Initial Commit

---
+++ SwiftUI-HW1/SwiftUI_HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HW1App.swift
+//  SwiftUI-HW1
+//
+//  Created by Михаил Светлов on 11.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
